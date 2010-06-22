2f7bec93ea833f145436cee57dbcc39d3d3e24d7
Santtu Pajukanta <santtu@pajukanta.fi>
2010-06-23 00:33:02 +0300

    renumber: also sort accounts

--- renumber.py
+++ renumber.py
@@ -8,6 +8,18 @@ import sys
 def sort_events(events):
     events.sort(key=lambda x: (x.date, x.number))
 
+def sort_accounts(accounts):
+    assets, liabilities, result = accounts
+    recursively_sort_subaccounts(assets)
+    recursively_sort_subaccounts(liabilities)
+    recursively_sort_subaccounts(result)
+
+def recursively_sort_subaccounts(account):
+    account.subaccounts.sort(key=lambda x: x.number)
+
+    for subaccount in account.subaccounts:
+        recursively_sort_subaccounts(subaccount)
+
 def renumber_events(events, start=1):
     for num, event in enumerate(events, start=start):
         event.number = num
@@ -16,6 +28,7 @@ def main(input_filename, output_filename):
     with open(input_filename, "rb") as input_file:
         with open(output_filename, "wb") as output_file:
             document = Parser(Lexer().lex_file(input_file)).parse_document()
+            sort_accounts(document.accounts)
             sort_events(document.events)
             renumber_events(document.events)
             Writer(output_file).write_document(document)
