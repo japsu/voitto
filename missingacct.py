f05d57d48f7e58bf89c524b7116400ce43c9b656
Santtu Pajukanta <santtu@pajukanta.fi>
2012-02-20 09:42:02 +0200

    missingacct: print accounts that are in some but not all input files

---
+++ missingacct.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# vim: shiftwidth=4 expandtab
+
+from collections import defaultdict
+from tappio import read_file, write_file
+import sys
+
+ONLY=True
+
+def flatten_accounts(accounts):
+    flat_accounts = dict()
+
+    for branch in accounts:
+        _flatten_accounts(branch, flat_accounts)
+
+    return flat_accounts
+
+def _flatten_accounts(account, accounts):
+    accounts[account.number] = account
+
+    for subaccount in account.subaccounts:
+        _flatten_accounts(subaccount, accounts)
+
+def main(*input_filenames):
+    input_filenames = set(input_filenames)
+
+    documents = ((filename, read_file(filename)) for filename in input_filenames)
+    flat_accounts = dict((filename, flatten_accounts(document.accounts)) for (filename, document) in documents)
+
+    all_accounts = dict()
+    map(all_accounts.update, flat_accounts.itervalues())
+    
+    havity = defaultdict(set)
+    for account_num, account in all_accounts.iteritems():
+        for filename, accounts in flat_accounts.iteritems():
+            if account_num in accounts:
+                havity[account_num].add(filename)
+
+    for account_num, filenames in havity.iteritems():
+        fmt_filenames = " ".join(filenames)
+
+        if filenames == input_filenames:
+            if not ONLY:
+                print "{account_num}: ALL  {fmt_filenames}".format(**locals())
+        else:
+            print "{account_num}: ONLY {fmt_filenames}".format(**locals())
+            
+
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
